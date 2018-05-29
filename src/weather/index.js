c1f3ebfe7829a3ff7e260b67bce50f8a256dc5ac
Joshua Schmidt <jschmidtnj@gmail.com>
2018-05-29 16:12:40 -0400

    Update index.js

--- src/weather/index.js
+++ src/weather/index.js
@@ -1,5 +1,4 @@
 let request = require('request');
-let city = 'hoboken';
 
 /**
 * Gets the next practices as a formatted string
@@ -11,34 +10,40 @@ let city = 'hoboken';
 var getWeather = function(option, callback){
   //Load client secrets from a local file.
   fs.readFile('credentials/weather_secret.json', function processClientSecrets(error, content) {
-      if(error){
-          callback(error,null);
-          return;
-      }
-      // Authorize a client with the loaded credentials, then call the
-      // OpenWeatherMap API.
-      else{
-        let apiKey = JSON.parse(content)
-        if(option == 0){
-          let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-        }
-        else if(option == 1){
-          let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
-        }
-        else{
-          //7 days, imperial units, by city
-          let url = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=${apiKey}&units=imperial&cnt=7`
-        }
+    if(error){
+      callback(error,null);
+      return;
+    }
+    // Authorize a client with the loaded credentials, then call the
+    // OpenWeatherMap API.
+    else{
+      function getData(url){
         request(url, function (err, response, body) {
           if(err){
             callback('Error with key: ' + error, null);
           }
           else {
             let weather = JSON.parse(body)
-            let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-            console.log(message);
+            return weather;
           }
         }
+      }
+      let apiKey = JSON.parse(content)
+      if(option == 0){
+        let city = 'hoboken';
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+        callback(null, getData(url));
+      }
+      else if(option == 1){
+        let city = 'hoboken';
+        let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+        callback(null, getData(url));
+      }
+      else{
+        //7 days, imperial units, by city
+        let city = 'hoboken';
+        let url = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=${apiKey}&units=imperial&cnt=7`
+        callback(null, getData(url));
       }
     }
   });
